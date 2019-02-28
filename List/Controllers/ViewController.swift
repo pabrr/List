e6bfc7ddd21c97d65ead1ed39eeff9ddec20ded8
Polina Abrosimova <abrosimowa97@gmail.com>
2019-02-28 14:53:51 +0500

    Begin

---
+++ List/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by Polina Abrosimova on 28/02/2019.
+//  Copyright © 2019 Polina Abrosimova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '12.0'
+
+target 'List' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+    pod 'SnapKit', '~> 4.0.0'
+
+end
